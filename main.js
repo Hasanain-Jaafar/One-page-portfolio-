8c806e5c99471343038332c1cfa42f2935d5a5a6
RedFlyDragon <hassanainadm@gmail.com>
2022-02-04 21:47:35 +0100

    created HTML file and CSS with a custimization on CSS

---
+++ main.js
@@ -0,0 +1,16 @@
+/*let welcoming = document.getElementById("introduction")
+
+welcoming.onmouseover= function(){
+
+    welcoming.innerHTML="Welcome To My Website";
+    welcoming.style.color="yellow";
+}
+
+
+welcoming.onmouseout= function(){
+    
+    welcoming.innerHTML="I'm Saying";
+    welcoming.style.color="rgb(61, 61, 240)"
+    
+    
+}*/
